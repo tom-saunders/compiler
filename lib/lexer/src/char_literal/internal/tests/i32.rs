1b99ebef3eb049ff6e5a16a9bbc00f3f3437421d
Tom Saunders <tom@paradoxic.al>
2024-06-17 12:50:47 +0100

    Cover all the hex inputs.

--- lib/lexer/src/char_literal/internal/tests/i32.rs
+++ lib/lexer/src/char_literal/internal/tests/i32.rs
@@ -9,7 +9,7 @@ use crate::universal_char::univ_esc_impl;
 use crate::Token;
 use crate::CharLit_U;
 
-fn actual_i32(input: &str) -> Token {
+fn actual(input: &str) -> Token {
     let location = Box::new(TestLocation);
     let text = text_state_impl_i32(input.chars().peekable());
 
@@ -29,29 +29,29 @@ fn actual_i32(input: &str) -> Token {
     char_lit.consume_char_literal()
 }
 
-fn unknown_and_actual_i32(input: &str) -> (Token, Token) {
+fn unknown_and_actual(input: &str) -> (Token, Token) {
     let expected = Token::Unknown(input.to_string());
-    let actual = actual_i32(input);
+    let actual = actual(input);
 
     (expected, actual)
 }
 
-macro_rules! exp_and_actual_i32 {
+macro_rules! exp_and_actual {
     ($v: literal, $input: literal) => {
-        (CharLit_U($v as i32), actual_i32($input))
+        (CharLit_U($v as i32), actual($input))
     };
 }
 
 #[test]
 fn test_i32_char_literal_empty() {
-    let (expected, actual) = unknown_and_actual_i32("''");
+    let (expected, actual) = unknown_and_actual("''");
 
     assert_eq!(expected, actual)
 }
 
 #[test]
 fn test_i32_char_literals_unterminated_eoi() {
-    let (expected, actual) = unknown_and_actual_i32("'");
+    let (expected, actual) = unknown_and_actual("'");
 
     assert_eq!(expected, actual)
 }
@@ -59,14 +59,14 @@ fn test_i32_char_literals_unterminated_eoi() {
 #[test]
 fn test_i32_char_literals_unterminated_eol() {
     let expected = Token::Unknown("'".to_string());
-    let actual = actual_i32("'\n");
+    let actual = actual("'\n");
 
     assert_eq!(expected, actual)
 }
 
 #[test]
 fn test_i32_char_literal_1_1byte_utf8_char() {
-    let (expected, actual) = exp_and_actual_i32!('a', "'a'");
+    let (expected, actual) = exp_and_actual!('a', "'a'");
     let CharLit_U(e) = expected else {
         panic!()
     };
@@ -79,7 +79,7 @@ fn test_i32_char_literal_1_1byte_utf8_char() {
 
 #[test]
 fn test_i32_char_literal_2_1byte_utf8_char() {
-    let (expected, actual) = exp_and_actual_i32!('b', "'ab'");
+    let (expected, actual) = exp_and_actual!('b', "'ab'");
     let CharLit_U(e) = expected else {
         panic!()
     };
@@ -92,7 +92,7 @@ fn test_i32_char_literal_2_1byte_utf8_char() {
 
 #[test]
 fn test_i32_char_literal_3_1byte_utf8_char() {
-    let (expected, actual) = exp_and_actual_i32!('c', "'abc'");
+    let (expected, actual) = exp_and_actual!('c', "'abc'");
     let CharLit_U(e) = expected else {
         panic!()
     };
@@ -105,7 +105,7 @@ fn test_i32_char_literal_3_1byte_utf8_char() {
 
 #[test]
 fn test_i32_char_literal_4_1byte_utf8_char() {
-    let (expected, actual) = exp_and_actual_i32!('d', "'abcd'");
+    let (expected, actual) = exp_and_actual!('d', "'abcd'");
     let CharLit_U(e) = expected else {
         panic!()
     };
@@ -118,7 +118,7 @@ fn test_i32_char_literal_4_1byte_utf8_char() {
 
 #[test]
 fn test_i32_char_literal_5_1byte_utf8_char() {
-    let (expected, actual) = exp_and_actual_i32!('e', "'abcde'");
+    let (expected, actual) = exp_and_actual!('e', "'abcde'");
     let CharLit_U(e) = expected else {
         panic!()
     };
@@ -131,7 +131,7 @@ fn test_i32_char_literal_5_1byte_utf8_char() {
 
 #[test]
 fn test_i32_char_literal_1_2byte_utf8_char() {
-    let (expected, actual) = exp_and_actual_i32!(0xa0, "'\u{00a0}'");
+    let (expected, actual) = exp_and_actual!(0xa0, "'\u{00a0}'");
     let CharLit_U(e) = expected else {
         panic!()
     };
@@ -144,7 +144,7 @@ fn test_i32_char_literal_1_2byte_utf8_char() {
 
 #[test]
 fn test_i32_char_literal_2_2byte_utf8_char() {
-    let (expected, actual) = exp_and_actual_i32!(0xa1, "'\u{00a0}\u{00a1}'");
+    let (expected, actual) = exp_and_actual!(0xa1, "'\u{00a0}\u{00a1}'");
     let CharLit_U(e) = expected else {
         panic!()
     };
@@ -157,7 +157,7 @@ fn test_i32_char_literal_2_2byte_utf8_char() {
 
 #[test]
 fn test_i32_char_literal_3_2byte_utf8_char() {
-    let (expected, actual) = exp_and_actual_i32!(0xa2, "'\u{00a0}\u{00a1}\u{00a2}'");
+    let (expected, actual) = exp_and_actual!(0xa2, "'\u{00a0}\u{00a1}\u{00a2}'");
     let CharLit_U(e) = expected else {
         panic!()
     };
@@ -170,7 +170,7 @@ fn test_i32_char_literal_3_2byte_utf8_char() {
 
 #[test]
 fn test_i32_char_literal_1_3byte_utf8_char() {
-    let (expected, actual) = exp_and_actual_i32!(0x0800, "'\u{0800}'");
+    let (expected, actual) = exp_and_actual!(0x0800, "'\u{0800}'");
     let CharLit_U(e) = expected else {
         panic!()
     };
@@ -183,7 +183,7 @@ fn test_i32_char_literal_1_3byte_utf8_char() {
 
 #[test]
 fn test_i32_char_literal_2_3byte_utf8_char() {
-    let (expected, actual) = exp_and_actual_i32!(0x0801, "'\u{0800}\u{0801}'");
+    let (expected, actual) = exp_and_actual!(0x0801, "'\u{0800}\u{0801}'");
     let CharLit_U(e) = expected else {
         panic!()
     };
@@ -196,7 +196,7 @@ fn test_i32_char_literal_2_3byte_utf8_char() {
 
 #[test]
 fn test_i32_char_literal_1_4byte_utf8_char() {
-    let (expected, actual) = exp_and_actual_i32!(0x010000, "'\u{010000}'");
+    let (expected, actual) = exp_and_actual!(0x010000, "'\u{010000}'");
     let CharLit_U(e) = expected else {
         panic!()
     };
@@ -209,7 +209,248 @@ fn test_i32_char_literal_1_4byte_utf8_char() {
 
 #[test]
 fn test_i32_char_literal_2_4byte_utf8_char() {
-    let (expected, actual) = exp_and_actual_i32!(0x010001, "'\u{010000}\u{010001}'");
+    let (expected, actual) = exp_and_actual!(0x010001, "'\u{010000}\u{010001}'");
+    let CharLit_U(e) = expected else {
+        panic!()
+    };
+    let CharLit_U(a) = actual else {
+        panic!()
+    };
+
+    assert_eq!(expected, actual, "{e:#010x} != {a:#010x}")
+}
+
+#[test]
+fn test_i32_char_literal_hex_0digit() {
+    let (expected, actual) = unknown_and_actual(r"'\x'");
+
+    assert_eq!(expected, actual)
+}
+
+#[test]
+fn test_i32_char_literal_hex_low_1digit() {
+    let (expected, actual) = exp_and_actual!(0x06, r"'\x6'");
+    let CharLit_U(e) = expected else {
+        panic!()
+    };
+    let CharLit_U(a) = actual else {
+        panic!()
+    };
+
+    assert_eq!(expected, actual, "{e:#010x} != {a:#010x}")
+}
+
+#[test]
+fn test_i32_char_literal_hex_low_2digit() {
+    let (expected, actual) = exp_and_actual!(0x66, r"'\x66'");
+    let CharLit_U(e) = expected else {
+        panic!()
+    };
+    let CharLit_U(a) = actual else {
+        panic!()
+    };
+
+    assert_eq!(expected, actual, "{e:#010x} != {a:#010x}")
+}
+
+#[test]
+fn test_i32_char_literal_hex_low_3digit() {
+    let (expected, actual) = exp_and_actual!(0x666, r"'\x666'");
+    let CharLit_U(e) = expected else {
+        panic!()
+    };
+    let CharLit_U(a) = actual else {
+        panic!()
+    };
+
+    assert_eq!(expected, actual, "{e:#010x} != {a:#010x}")
+}
+
+#[test]
+fn test_i32_char_literal_hex_low_4digit() {
+    let (expected, actual) = exp_and_actual!(0x6666, r"'\x6666'");
+    let CharLit_U(e) = expected else {
+        panic!()
+    };
+    let CharLit_U(a) = actual else {
+        panic!()
+    };
+
+    assert_eq!(expected, actual, "{e:#010x} != {a:#010x}")
+}
+
+#[test]
+fn test_i32_char_literal_hex_low_5digit() {
+    let (expected, actual) = exp_and_actual!(0x066666, r"'\x66666'");
+    let CharLit_U(e) = expected else {
+        panic!()
+    };
+    let CharLit_U(a) = actual else {
+        panic!()
+    };
+
+    assert_eq!(expected, actual, "{e:#010x} != {a:#010x}")
+}
+
+#[test]
+fn test_i32_char_literal_hex_low_6digit() {
+    let (expected, actual) = exp_and_actual!(0x666666, r"'\x666666'");
+    let CharLit_U(e) = expected else {
+        panic!()
+    };
+    let CharLit_U(a) = actual else {
+        panic!()
+    };
+
+    assert_eq!(expected, actual, "{e:#010x} != {a:#010x}")
+}
+
+#[test]
+fn test_i32_char_literal_hex_low_7digit() {
+    let (expected, actual) = exp_and_actual!(0x06666666, r"'\x6666666'");
+    let CharLit_U(e) = expected else {
+        panic!()
+    };
+    let CharLit_U(a) = actual else {
+        panic!()
+    };
+
+    assert_eq!(expected, actual, "{e:#010x} != {a:#010x}")
+}
+
+#[test]
+fn test_i32_char_literal_hex_low_8digit() {
+    let (expected, actual) = exp_and_actual!(0x66666666, r"'\x66666666'");
+    let CharLit_U(e) = expected else {
+        panic!()
+    };
+    let CharLit_U(a) = actual else {
+        panic!()
+    };
+
+    assert_eq!(expected, actual, "{e:#010x} != {a:#010x}")
+}
+
+#[test]
+fn test_i32_char_literal_hex_low_9digit() {
+    let (expected, actual) = exp_and_actual!(0x66666666, r"'\x666666666'");
+    let CharLit_U(e) = expected else {
+        panic!()
+    };
+    let CharLit_U(a) = actual else {
+        panic!()
+    };
+
+    assert_eq!(expected, actual, "{e:#010x} != {a:#010x}")
+}
+
+#[test]
+fn test_i32_char_literal_hex_high_1digit() {
+    let (expected, actual) = exp_and_actual!(0x0f, r"'\xf'");
+    let CharLit_U(e) = expected else {
+        panic!()
+    };
+    let CharLit_U(a) = actual else {
+        panic!()
+    };
+
+    assert_eq!(expected, actual, "{e:#010x} != {a:#010x}")
+}
+
+#[test]
+fn test_i32_char_literal_hex_high_2digit() {
+    let (expected, actual) = exp_and_actual!(0xff, r"'\xff'");
+    let CharLit_U(e) = expected else {
+        panic!()
+    };
+    let CharLit_U(a) = actual else {
+        panic!()
+    };
+
+    assert_eq!(expected, actual, "{e:#010x} != {a:#010x}")
+}
+
+#[test]
+fn test_i32_char_literal_hex_high_3digit() {
+    let (expected, actual) = exp_and_actual!(0x0fff, r"'\xfff'");
+    let CharLit_U(e) = expected else {
+        panic!()
+    };
+    let CharLit_U(a) = actual else {
+        panic!()
+    };
+
+    assert_eq!(expected, actual, "{e:#010x} != {a:#010x}")
+}
+
+#[test]
+fn test_i32_char_literal_hex_high_4digit() {
+    let (expected, actual) = exp_and_actual!(0xffff, r"'\xffff'");
+    let CharLit_U(e) = expected else {
+        panic!()
+    };
+    let CharLit_U(a) = actual else {
+        panic!()
+    };
+
+    assert_eq!(expected, actual, "{e:#010x} != {a:#010x}")
+}
+
+#[test]
+fn test_i32_char_literal_hex_high_5digit() {
+    let (expected, actual) = exp_and_actual!(0x0fffff, r"'\xfffff'");
+    let CharLit_U(e) = expected else {
+        panic!()
+    };
+    let CharLit_U(a) = actual else {
+        panic!()
+    };
+
+    assert_eq!(expected, actual, "{e:#010x} != {a:#010x}")
+}
+
+#[test]
+fn test_i32_char_literal_hex_high_6digit() {
+    let (expected, actual) = exp_and_actual!(0xffffff, r"'\xffffff'");
+    let CharLit_U(e) = expected else {
+        panic!()
+    };
+    let CharLit_U(a) = actual else {
+        panic!()
+    };
+
+    assert_eq!(expected, actual, "{e:#010x} != {a:#010x}")
+}
+
+#[test]
+fn test_i32_char_literal_hex_high_7digit() {
+    let (expected, actual) = exp_and_actual!(0x0fffffff, r"'\xfffffff'");
+    let CharLit_U(e) = expected else {
+        panic!()
+    };
+    let CharLit_U(a) = actual else {
+        panic!()
+    };
+
+    assert_eq!(expected, actual, "{e:#010x} != {a:#010x}")
+}
+
+#[test]
+fn test_i32_char_literal_hex_high_8digit() {
+    let (expected, actual) = exp_and_actual!(0xffffffff_u32, r"'\xffffffff'");
+    let CharLit_U(e) = expected else {
+        panic!()
+    };
+    let CharLit_U(a) = actual else {
+        panic!()
+    };
+
+    assert_eq!(expected, actual, "{e:#010x} != {a:#010x}")
+}
+
+#[test]
+fn test_i32_char_literal_hex_high_9digit() {
+    let (expected, actual) = exp_and_actual!(0xffffffff_u32, r"'\xfffffffff'");
     let CharLit_U(e) = expected else {
         panic!()
     };
